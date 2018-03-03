aa2cc483f23a4e44d49b3f491813f1cbaae96fd6
Apurv Asthana <35286370+ApurvAsthana@users.noreply.github.com>
2018-03-03 08:03:42 -0800

    DAO calls

--- src/Controller/Controller.py
+++ src/Controller/Controller.py
@@ -1,19 +1,21 @@
 from flask import Flask, jsonify
 from flask import make_response
-from
+from dao import image,recipe
+imageDA = image()
+recipeDA = recipe()
 
 handler = Flask(__name__)
-@handler.route('/recipe/search')
+@handler.route('/recipe/search',methods=['POST'])
 def search():
-    return ""
+    return recipeDA.get_recipe_with_ingredients()
 
-@handler.route('/recipe/search/image')
+@handler.route('/recipe/search/image',methods=['POST'])
 def searchImages():
-    return ""
+    return recipeDA.get_recipe_with_image()
 
-@handler.route('/recipe/search/ingredients')
+@handler.route('/recipe/search/ingredients',methods=['POST'])
 def searchIngredients():
-    return ""
+    return imageDA.get_image_ingredients()
 
 @handler.errorhandler(404)
 def notfound(error):
